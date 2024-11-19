3cd79fdbf3ed9906b678080bdd71a2da763a95d1
JackelinBlanquiceth <glozanob@unal.edu.co>
2024-11-19 01:35:15 -0500

    .

--- televisores/control.py
+++ televisores/control.py
@@ -1,39 +1,42 @@
-from __future__ import annotations
-class Control:
-    def __init__(self) -> None: 
-        self._tv = None    
+from tv import TV
 
+class Control:
+    def __init__(self):
+        self._tv = None
+    
     def enlazar(self, tv):
-        self._tv = tv
+        self._tv = tv 
         tv.setControl(self)
 
-    def turnOn(self) -> None:
+    def turnOn(self):
         self._tv.turnOn()
-
-    def turnOff(self) -> None:
-        self._tv.turnOff()
-
-    def canalUp(self) -> None:
-        self._tv.canalUp()
     
-    def canalDown(self) -> None:
+    def turnOff(self):
+        self._tv.turnOff()
+    
+    def canalUp(self):
+        self._tv.canalUp()
+
+    def canalDown(self):
         self._tv.canalDown()
 
-    def volumenUp(self) -> None:
+    def volumenUp(self):
         self._tv.volumenUp()
-
-    def volumenDown(self) -> None:
+    
+    def volumenDown(self):
         self._tv.volumenDown()
 
-    def setCanal(self, canal: int) -> None:
+    def setCanal(self, canal: int):
         self._tv.setCanal(canal)
 
-    def setVolumen(self, volumen: int) -> None:
+    def setVolumen(self, volumen: int):
         self._tv.setVolumen(volumen)
 
-    def getTv(self) -> TV:
+    def getTv(self):
+        self._tv = TV
         return self._tv
     
-    def setTv(self, tv: TV) -> None:
+    def setTv(self, tv: TV):
         self._tv = tv
     
+  
--- televisores/marca.py
+++ televisores/marca.py
@@ -1,12 +1,8 @@
 class Marca:
     def __init__(self, nombre:str):
         self._nombre = nombre
-        return None
-
     def getNombre(self):
         self._nombre = str
-        return self._nombre
-    
     def setNombre(self, nombre:str):
         self._nombre = nombre
         return None
