bbaf565a75a1e68bcdecd95c2d8c21fdcc6b3fad
JackelinBlanquiceth <glozanob@unal.edu.co>
2024-11-19 01:05:18 -0500

    Update marca.py

--- televisores/marca.py
+++ televisores/marca.py
@@ -1,9 +1,12 @@
 class Marca:
     def __init__(self, nombre:str):
+        nombre = None
         self._nombre = nombre
 
     def getNombre(self):
+        self._nombre = str
         return self._nombre
     
     def setNombre(self, nombre:str):
+        nombre = None
         self._nombre = nombre
