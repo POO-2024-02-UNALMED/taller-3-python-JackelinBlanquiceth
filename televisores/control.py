ce147c913942e27cf23e4536cac3f89e226cd001
JackelinBlanquiceth <glozanob@unal.edu.co>
2024-11-19 01:26:28 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -1,48 +1,42 @@
 from __future__ import annotations
-from tv import TV
+
 class Control:
-    def __init__(self):
+    def __init__(self) -> None:
         self._tv = None
+    
+    def enlazar(self, tv):
+        self._tv = tv 
+        tv.setControl(self)
 
     def turnOn(self):
-        if self._tv:
-         self._tv.turnOn()
+        self._tv.turnOn()
     
     def turnOff(self):
-        if self._tv:
-         self._tv.turnOff()
+        self._tv.turnOff()
     
     def canalUp(self):
-        if self._tv:
-         self._tv.canalUp()
+        self._tv.canalUp()
 
     def canalDown(self):
-        if self._tv:
-         self._tv.canalDown()
+        self._tv.canalDown()
 
     def volumenUp(self):
-        if self._tv: 
-         self._tv.volumenUp()
+        self._tv.volumenUp()
     
     def volumenDown(self):
-        if self._tv:
-         self._tv.volumenDown()
+        self._tv.volumenDown()
 
-    def setCanal(self, canal:int):
-       if self._tv: 
+    def setCanal(self, canal: int):
         self._tv.setCanal(canal)
 
-    def setVolumen(self, volumen:int):
-       if self._tv: 
+    def setVolumen(self, volumen: int):
         self._tv.setVolumen(volumen)
 
     def getTv(self):
+        self._tv = TV
         return self._tv
     
-    def setTv(self, tv:TV):
+    def setTv(self, tv: TV):
         self._tv = tv
     
-    def enlazar(self, tv):
-      if isinstance(tv, TV):
-        self._tv = tv 
-        tv.setControl(self)
+  
